4a5acd68cf097def6f7ca6c2a4c4983f920622d9
Scott Vaughan <scott.vaughan@ensono.com>
2023-02-13 14:48:33 +0000

    Update

--- ModernApiVersioning/Program.cs
+++ ModernApiVersioning/Program.cs
@@ -6,17 +6,13 @@ builder.Services.AddApiVersioning(options =>
 {
     options.DefaultApiVersion = new ApiVersion(2,1);
     options.AssumeDefaultVersionWhenUnspecified = true;
-    //options.ApiVersionReader = new QueryStringApiVersionReader();
-    //options.ApiVersionReader = new MediaTypeApiVersionReader("version");
     options.ApiVersionReader = new HeaderApiVersionReader("x-api-version");
-    //options.ApiVersionReader = new UrlSegmentApiVersionReader();
 });
 
 var app = builder.Build();
 
 var versionSet = app.NewApiVersionSet()
     .HasApiVersion(new ApiVersion(1,0))
-    .HasApiVersion(new ApiVersion(2,0))
     .HasApiVersion(new ApiVersion(2,1))
     .HasDeprecatedApiVersion(new ApiVersion(1,0))
     .ReportApiVersions()
@@ -29,10 +25,6 @@ app.MapGet("hello", (HttpContext context) =>
 })
     .WithApiVersionSet(versionSet)
     .MapToApiVersion(1,0);
-
-app.MapGet("hello", () => "Hello World! v2.0")
-    .WithApiVersionSet(versionSet)
-    .MapToApiVersion(2, 0);
 
 app.MapGet("hello", () => "Hello World! v2.1")
     .WithApiVersionSet(versionSet)
